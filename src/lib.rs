e190de4650f9ae2c83a061990b5db6b5ed1a1e8b
RonaldoDairo <programaciondairo@gmail.com>
2023-12-18 23:41:24 +0100

    [ADD]testing the updated smart contract

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 use gstd::{msg, prelude::*, debug};
-
+/*
 #[no_mangle]
 extern "C" fn handle() {
     msg::reply(String::from("Hello"), 0)
@@ -12,4 +12,30 @@ extern "C" fn init() {
     let init_message: String = msg::load()
         .expect("Can't load init message");
     debug!("Program was initialized with message {:?}", init_message);
+}*/
+
+static mut GREETING: Option<String> = None;
+
+#[no_mangle]
+extern "C" fn handle() {
+    let input_message: InputMessages = msg::load()
+        .expect("Error in loading InputMessages");
+    let greeting = unsafe {
+        GREETING
+            .as_mut()
+            .expect("The contract is not initialized")
+    };
+    match input_message {
+        InputMessages::SendHelloTo(account) => {
+            debug!("Message: SendHelloTo {:?}", account);
+            msg::send(account, greeting, 0)
+                .expect("Error in sending Hello message to account");
+        }
+        InputMessages::SendHelloReply => {
+            debug!("Message: SendHelloReply");
+            msg::reply(greeting, 0)
+                .expect("Error in sending reply");
+        }
+    }
 }
+
